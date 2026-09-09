1d06efa0ffe3c357577f0d950bc9c72958459580
Alejandro Parra <alejandroparral@gmail.com>
2026-09-09 18:15:05 -0500

    Bump version code and version name to 1.00.003 (#35)
    * Bump version code and version name to 1.00.003
    
    * Update version code and name to 1.00.003

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "co.japl.android.synapsefit"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1_00_002
-        versionName = "1.00.002"
+        versionCode = 1_00_003
+        versionName = "1.00.003"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
