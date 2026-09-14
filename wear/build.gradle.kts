5f307f21f18c4eae4518b6c55e9a75e9c36501a7
Alejandro Parra <alejandroparral@gmail.com>
2026-09-14 10:32:38 -0500

    Fix formatting in README header (#63)
    * Fix formatting in README header
    
    * chore(release): bump version to 1.00.009Integracion entre wear y app (2)
    
    * Update versionCode and versionName in build.gradle.kts
    
    * Update build.gradle.kts
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "co.japl.android.synapsefit"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 21_00_009
-        versionName = "1.00.009"
+        versionCode = 21_00_010
+        versionName = "1.00.010 Integracion entre wear y app"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
