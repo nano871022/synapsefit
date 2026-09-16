9e01efcaa8312e41a4695ee7c1df4bfcca1d3aa6
Jose Alejandro Parra Lasso <alejandro.parralasso@xpo.com>
2026-09-16 15:51:01 -0500

    Update UI wear and add preview interfaces to wear to check implementations

--- app/src/main/java/co/japl/android/synapsefit/app/controller/workout/ActiveWorkoutSessionViewModel.kt
+++ app/src/main/java/co/japl/android/synapsefit/app/controller/workout/ActiveWorkoutSessionViewModel.kt
@@ -181,7 +181,7 @@ class ActiveWorkoutSessionViewModel(
                             cooldownTargetTimestamp = event.cooldownTargetTimestamp,
                         )
                     }
-                    event.cooldownTargetTimestamp?.let{
+                    event.cooldownTargetTimestamp?.let {
                         val diff = it - System.currentTimeMillis()
                         val remainingSec = if (diff > 0) (diff / 1000L).toInt() else 0
                         startRestTimer(remainingSec, event.cooldownTargetTimestamp)
